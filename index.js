6b24cc96bbdf271c7aa1755972e6e8fa2b5cf495
j-stax <ahnjames01@gmail.com>
2025-03-10 21:29:05 -0400

    Tags route done.

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import tagsRoute from './routes/tags.js'
 
-const app = express()
+const app = express();
 
 const logRequest = (req, res, next) => {
-    console.log(`Request: ${req.method} for ${req.path} on ${new Date()}`)
-    next()
+    console.log(`Request: ${req.method} for ${req.path} on ${new Date()}`);
+    next();
 }
 
 // 1. What else should we gitignore? .env
 // 2. What about comments.json? Include it in the photos.json
 // 3. How are we supposed to use the public/images/ http://localhost8080/public/images/...
 
-app.use(logRequest)
+app.use(logRequest);
 
-const { PORT, CORS_ORIGIN } = process.env
-console.log(PORT)
+const { PORT, CORS_ORIGIN } = process.env;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({ origin: CORS_ORIGIN }))
+app.use(cors({ origin: CORS_ORIGIN }));
 
 app.get('/photos', (req, res) => {
     res.send("<h1>Hello World</h1>")
-})
+});
+
+app.use('/tags', tagsRoute)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
     console.log("Press CTRL + C to stop server")
-})
+});
