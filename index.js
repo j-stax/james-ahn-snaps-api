0e1bb5c768ef0d4e25b00916f3f5666726b6b806
j-stax <ahnjames01@gmail.com>
2025-03-11 21:13:01 -0400

    Make photo data URL point to static server files for static image requests.

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import tagsRoute from './routes/tags.js'
-import photosRoutes from './routes/photos.js'
+import tagsRoute from './routes/tags.js';
+import photosRoutes from './routes/photos.js';
+
+const { PORT, CORS_ORIGIN } = process.env;
 
 const app = express();
 
 const logRequest = (req, res, next) => {
-    console.log(`Request: ${req.method} for ${req.path} on ${new Date()}`);
+    if (!req.path.includes("images")) {
+        console.log(`Request: ${req.method} for ${req.path} on ${new Date()}`);
+    }
     next();
 }
 
-// 3. How are we supposed to use the public/images/ http://localhost8080/public/images/...
+app.use(express.json());
 
 app.use(logRequest);
 
-const { PORT, CORS_ORIGIN } = process.env;
-
 app.use(cors({ origin: CORS_ORIGIN }));
 
-app.use(express.json());
+app.use(express.static("public"));
 
-app.use('/tags', tagsRoute)
+app.use('/tags', tagsRoute);
 
-app.use('/photos', photosRoutes)
+app.use('/photos', photosRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
