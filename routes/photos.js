86bcdcbfd4c05363560a8032a287b214739c83e5
j-stax <ahnjames01@gmail.com>
2025-03-12 22:37:01 -0400

    Add likes and comments data to photos.json; Add comments to read/write functions to photos.json in photos.js.

--- routes/photos.js
+++ routes/photos.js
@@ -3,11 +3,13 @@ import { v4 as uuidv4 } from 'uuid';
 import express from 'express';
 const router = express.Router();
 
+// Read from photos.json file and convert to a js data structure
 const readPhotos = () => {
-    const jsonPhotos = JSON.parse(fs.readFileSync('./data/photos.json'));
-    return jsonPhotos;
+    const photosData = JSON.parse(fs.readFileSync('./data/photos.json'));
+    return photosData;
 }
 
+// Write to photos.json file with data
 const writePhotos = (data) => {
     const stringifiedData = JSON.stringify(data);
     fs.writeFileSync('./data/photos.json', stringifiedData)
@@ -36,6 +38,8 @@ router.get('/:id', (req, res) => {
         photo: photo.photo,
         photoDescription: photo.photoDescription,
         photographer: photo.photographer,
+        likes: photo.likes,
+        timestamp: photo.timestamp,
         tags: photo.tags
     }
     res.json(photoExComments);
@@ -65,7 +69,7 @@ router.post('/:id/comments', (req, res) => {
         }
     })
     // writePhotos(photosData)
-    console.log(photosData[0])
+    console.log(photosData)
     res.json(newCommentObj)
 })
 
