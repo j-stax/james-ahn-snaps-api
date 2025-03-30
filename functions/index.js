71c6b5ca74746532e95405b969be5bc0a62c2963
j-stax <ahnjames01@gmail.com>
2025-03-30 11:17:41 -0400

    Change html string formatting in index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,17 @@
 export const handler = () => {
-    const html = "<html><body><div style=\"width: 50%; margin: 5% auto; text-align: center;\"><h1>Welcome to my Snaps project API!</h1><p>This is a simple Express server that I created for my BrainStation software engineering bootcamp " +
-    "photo sharing application. You can check out the source code <a href=\"https://github.com/j-stax/james-ahn-snaps-api\" target=\"_blank\">here.</a></p></div></body></html>";
+    const html = `
+        <html>
+            <body>
+                <div style="width: 50%; margin: 5% auto; text-align: center;">
+                    <h1>Welcome to my Snaps project API!</h1>
+                    <p>
+                        This is a simple Express server that I created for my BrainStation software engineering bootcamp photo sharing application. 
+                        You can check out the source code <a href="https://github.com/j-stax/james-ahn-snaps-api" target="_blank">here.</a>
+                    </p>
+                </div>
+            </body>
+        </html>
+    `;
 
     return {
         statusCode: 200,
