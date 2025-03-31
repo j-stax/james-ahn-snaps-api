107238789be7a32517a92d5c510d3a5e871b884d
j-stax <ahnjames01@gmail.com>
2025-03-31 17:48:20 -0400

    Move GET and POST comments handler functions into one file.

--- functions/photos-id-comments.js
+++ functions/photos-id-comments.js
@@ -1,9 +1,54 @@
 import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 import { getResponseHeaders } from '../utils/getResponseHeaders';
 
+
 const handler = async (event) => {
+    switch (event.httpMethod) {
+        case 'GET':
+            return await handleGetRequest(event);
+        case 'POST':
+            return await handlePostRequest(event);
+        default:
+            return {
+                statusCode: 405,
+                headers: getResponseHeaders(),
+                body: 'Request method type not allowed'
+            }
+    }
+} 
+
+const handleGetRequest = async (event) => {
+    try {
+        const filePath = path.join(__dirname, '../data/photos.json');
+        const urlSegments = event.rawUrl.split('/');
+        const id = urlSegments[urlSegments.length - 2];
+        let photosData = await fs.promises.readFile(filePath, 'utf-8');
+        photosData = JSON.parse(photosData);
+        const photo = photosData.find(photoObj => photoObj.id === id);
+
+        if (photo) {          
+            return {
+                statusCode: 200,
+                headers: getResponseHeaders(),
+                body: JSON.stringify(photo.comments),
+            };
+        } else {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: `Data not found for photo ID ${id}.` })
+            }
+        };  
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error reading photos." })
+        };
+    }
+} 
+
+const handlePostRequest = async (event) => {
     try {
         const filePath = path.join(__dirname, '../data/photos.json');
         const urlSegments = event.rawUrl.split('/');
