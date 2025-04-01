56c8d9f28fe91b27ac5d9532c987966159dd4441
j-stax <ahnjames01@gmail.com>
2025-04-01 09:12:46 -0400

    Debug comments POST handler.

--- functions/photos-id-comments-handler.js
+++ functions/photos-id-comments-handler.js
@@ -61,7 +61,7 @@ const handleGetRequest = async (event) => {
 
 const handlePostRequest = async (event) => {
     try {
-        const filePath = path.join(__dirname, '../data/photos.json');
+        const filePath = path.join(__dirname, './data/photos.json');
         const urlSegments = event.rawUrl.split('/');
         const id = urlSegments[urlSegments.length - 2];
         const { name, comment } = JSON.parse(event.body);
@@ -83,13 +83,19 @@ const handlePostRequest = async (event) => {
                 }
             }); 
 
-            await fs.promises.writeFile('../data/photos.json', JSON.stringify(photosData));
-
             return {
                 statusCode: 201,
                 headers: getResponseHeaders(),
-                body: JSON.stringify(newCommentObj)
+                body: JSON.stringify(filePath)
             };
+
+            // await fs.promises.writeFile(filePath, JSON.stringify(photosData));
+
+            // return {
+            //     statusCode: 201,
+            //     headers: getResponseHeaders(),
+            //     body: JSON.stringify(newCommentObj)
+            // };
         } else {
             return {
                 statusCode: 404,
