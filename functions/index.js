243a315430cbd2bed7c80fe85862758b78838bdf
j-stax <ahnjames01@gmail.com>
2025-04-01 12:23:34 -0400

    Update /functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ export const handler = async () => {
                     <p>
                         This is a simple Express server that I created for my BrainStation software engineering bootcamp photo sharing application. 
                         You can check out the source code <a href="https://github.com/j-stax/james-ahn-snaps-api" target="_blank">here.</a>
+                        And here is the <a href="https://snapsjsx.netlify.app">link</a> to the Snaps React front-end application.
                     </p>
                     <img style="width: 100%; border-radius: 4px;" src="https://snapsapi.netlify.app/images/snaps-cover.png" alt="Image of Snaps app." />
                 </div>
