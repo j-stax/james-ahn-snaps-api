1a78754fa22fc9ab001d2fc1d24ae1da1bb608a8
j-stax <ahnjames01@gmail.com>
2025-03-31 21:13:00 -0400

    Update toml

--- functions/photos-id-comments-handler.js
+++ functions/photos-id-comments-handler.js
@@ -10,7 +10,7 @@ const handler = async (event) => {
             return {
                 statusCode: 200,
                 headers: {
-                    'Access-Control-Allow-Origin': 'https://snapsjsx.netlify.app',
+                    'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Headers': 'Content-Type',
                     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
                 }
--- utils/getResponseHeaders.js
+++ utils/getResponseHeaders.js
@@ -1,6 +1,6 @@
 export const getResponseHeaders = () => {
     return {
-        'Access-Control-Allow-Origin': 'https://snapsjsx.netlify.app',
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Content-Type': 'application/json'
