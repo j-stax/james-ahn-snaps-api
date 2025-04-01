644a0373d179b879fcc1e95a5342a23cc2d14b72
j-stax <ahnjames01@gmail.com>
2025-04-01 15:13:48 -0400

    Update /functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ export const handler = async () => {
                         You can check out the source code <a href="https://github.com/j-stax/james-ahn-snaps-api" target="_blank">here.</a>
                         And here is the <a href="https://snapsjsx.netlify.app" target="_blank">link</a> to the Snaps React front-end application.
                     </p>
-                    <img style="width: 100%; border-radius: 4px;" src="https://snapsapi.netlify.app/images/snaps-cover.png" alt="Image of Snaps app." />
+                    <img style="width: 100%; border-radius: 4px;" src="https://snapsapi.netlify.app/images/snaps-cover-full.png" alt="Image of Snaps app." />
                 </div>
             </body>
         </html>
